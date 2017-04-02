97465778497d7b07b4bc1625a6010bf45584350a
David Herron <david@davidherron.com>
2017-04-01 21:24:06 -0700

    Conversion to AkashaRender

--- config.js
+++ config.js
@@ -1,92 +1,70 @@
 
-module.exports = {
-    
-    root_assets: [ 'assets' ],
-    root_layouts: [ 'layouts' ],    // Directory for layout files
-    root_partials: [ 'partials' ],  // Directory for partials
-    root_out: 'out',                // Rendered output goes here
-    root_docs: [ 'documents' ],     // Directory/ies of input files
-    
-    // Root URL for the site this will generate
-    // Change this to suite your site
-    root_url: "http://skeleton.akashacms.com", 
-    
-    doMinimize: false,
-    
-    // If you're using akashacms-tagged-content
-    /* taggedContent: {
-        pathIndexes: '/tags/',
-        header: "---\ntitle: @title@\nlayout: tagpage.html.ejs\n---\n<p>Pages with tag @tagName@</p>"
-    }, */
-    
-    // Change this to suit your own server
-    deploy_rsync: {
-        user: 'user-name',
-        host: 'example.akashacms.com',
-        dir:  'example.akashacms.com'
-    },
-    
-    // Add any needed Mahabhuta functions here
-    mahabhuta: [
-    ],
-    
-    data: {
-        // Any objects put here are available in templates as data
-        metarobots: "index,follow",
-        metaOGtype: "website",
-        metaOGsite_name: "AkashaCMS example website",
-        metasubject: "AkashaCMS",
-        metalanguage: "EN",
-    },
-    
-    // Stuff for Google
-    google: {
-        analyticsAccount: "UA-#########-##",
-        analyticsDomain: "example.akashacms.com",
-        // siteVerification: "....."
-    },
-    
-    headerScripts: {
-        stylesheets: [
-            { href: "/css/style.css", media: "screen" }
-        ],
-        javaScriptTop: [
-        ],
-        javaScriptBottom: [
-        ]
-    },
-    
-    // embeddables: {
-    //     youtubeKey: '... retrieve your own key'
-    // },
-    
-    funcs: {
-        // Any functions put here are available in templates as functions
-    },
-    
-    /* log4js: {
-		appenders: [
-			{ type: "console" }
-		],
-		replaceConsole: true,
-    	levels: {
-    		"find": "TRACE",
-    		"fileCache": "TRACE",
-    		"renderer": "TRACE",
-    		"builtin": "INFO",
-    		"akashacms": "TRACE",
-    		"embeddables": "INFO",
-    		"[all]": "INFO"
-    	}
-    }, */
-    
-    config: function(akasha) {
-        akasha.registerPlugins([
-            { name: 'akashacms-theme-bootstrap', plugin: require('akashacms-theme-bootstrap') },
-            { name: 'akashacms-embeddables', plugin: require('akashacms-embeddables') },
-            { name: 'akashacms-booknav', plugin: require('akashacms-booknav') },
-            { name: 'akashacms-base', plugin: require('akashacms-base') }
-        ]);
-    }
+'use strict';
 
-};
+const util    = require('util');
+const akasha  = require('akasharender');
+const async   = require('async');
+const cheerio = require('cheerio');
+const path    = require('path');
+
+const log    = require('debug')('akashacms-skeleton:configuration');
+const error  = require('debug')('akashacms-skeleton:error-configuration');
+
+const config = new akasha.Configuration();
+
+// Fill this with the URL of your site
+config.rootURL("https://skeleton.akashacms.com");
+
+config
+    .addAssetsDir('assets')
+    .addAssetsDir({
+        src: 'node_modules/bootstrap/dist',
+        dest: 'vendor/bootstrap'
+    })
+   .addAssetsDir({
+        src: 'node_modules/jquery/dist',
+        dest: 'vendor/jquery'
+    })
+    .addLayoutsDir('layouts')
+    .addDocumentsDir('documents')
+    .addPartialsDir('partials');
+
+config
+    .use(require('akashacms-theme-bootstrap'))
+    .use(require('akashacms-base'))
+    .use(require('akashacms-breadcrumbs'))
+    .use(require('akashacms-booknav'))
+    .use(require('akashacms-embeddables'))
+    .use(require('akashacms-tagged-content'));
+
+config.plugin("akashacms-base").generateSitemap(config, true);
+
+// Add any stylesheets or JavaScript here
+// The /vendor/jquery and /vendor/bootstrap files come from the corresponding
+// modules under node_modules.  The last comes from the assets directory.
+//
+// If you wish to use LESS, you instead add css/style.css.less under
+// the documents directory.  Remember that files in the assets directory
+// are copied as-is, while files in the documents directory may be processed
+// depending on the file extension.  The .css.less extension invokes the
+// LESS compiler.
+config
+    .addFooterJavaScript({ href: "/vendor/jquery/jquery.min.js" })
+    .addFooterJavaScript({ href: "/vendor/bootstrap/js/bootstrap.min.js"  })
+    .addStylesheet({       href: "/vendor/bootstrap/css/bootstrap.min.css" })
+    .addStylesheet({       href: "/vendor/bootstrap/css/bootstrap-theme.min.css" })
+    .addStylesheet({       href: "/css/style.css" });
+
+config.setMahabhutaConfig({
+    recognizeSelfClosing: true,
+    recognizeCDATA: true,
+    decodeEntities: true
+});
+
+config.plugin("akashacms-tagged-content")
+    .sortBy('title')
+    .headerTemplate("---\ntitle: @title@\nlayout: tagpage.html.ejs\n---\n<p>Pages with tag @tagName@</p>")
+    .tagsDirectory('/tags/');
+
+config.prepare();
+module.exports = config;
